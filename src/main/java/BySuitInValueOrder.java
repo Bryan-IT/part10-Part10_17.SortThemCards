136c4958080ce574ad0378acf9a008b61436e57b
Bryan Trinh <bry.trinh@gmail.com>
2021-10-05 17:35:41 -0400

    fixed BySuitInValueOrder method

--- src/main/java/BySuitInValueOrder.java
+++ src/main/java/BySuitInValueOrder.java
@@ -4,10 +4,9 @@ public class BySuitInValueOrder implements Comparator<Card> {
 
     @Override
     public int compare(Card c1, Card c2) {
-        if(c1.getValue() == c2.getValue()){
-            return c1.getSuit().ordinal() - c2.getSuit().ordinal();
+        if(c1.getSuit() == c2.getSuit()){
+            return c1.getValue() - c2.getValue();
         }
-        return c1.getValue() - c2.getValue();
-
+        return c1.getSuit().ordinal() - c2.getSuit().ordinal();
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,7 +5,10 @@ public class Main {
 
     public static void main(String[] args) {
         // test your code here
-
-
+        BySuitInValueOrder comparator = new BySuitInValueOrder();
+        Card first = new Card(4,Suit.HEART);
+        Card second = new Card(5,Suit.HEART);
+        int num  = comparator.compare(first,second);
+        System.out.println(num);
     }
 }
